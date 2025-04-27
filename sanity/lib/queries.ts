b9426ace3a9e45b8eaafea2818d8c1050e709a7b
Saba <godmode7616@gmail.com>
2025-04-27 21:48:26 +0400

    feat: add playlist schema and query, update page to fetch editor picks

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -92,3 +92,28 @@ export const GET_STARTUP_BY_ID = defineQuery(`
     }
   }
 `);
+
+export const PLAYLIST_BY_SLUG_QUERY = 
+  defineQuery(`*[_type == "playlist" && slug.current == $slug][0] {
+    _id,
+    title,
+    slug,
+    select[] -> {
+      _id,
+      _createdAt,
+      title,
+      slug,
+      author -> {
+        _id,
+        name,
+        slug,
+        image,
+        bio
+      },
+      views,
+      description,
+      category,
+      image,
+      pitch,
+    }
+  }`);
